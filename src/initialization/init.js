aa40441b2f18c999038d3ca597e047e9ad977354
tuyen-at-work <tuyen.at.work@gmail.com>
2025-01-04 15:13:12 +0700

    build: compile TS to JS

--- src/initialization/init.js
+++
@@ -1,7 +0,0 @@
-import { initVisualTestsFolder } from './generate-tests';
-import { updatePackageJson } from './update-package';
-export async function initRegressify() {
-    await initVisualTestsFolder();
-    await updatePackageJson();
-}
-//# sourceMappingURL=init.js.map

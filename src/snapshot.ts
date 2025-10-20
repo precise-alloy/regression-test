8cc633b29ed7806d7c967ca8973b157a9bbc20eb
tuyen-at-work <tuyen.at.work@gmail.com>
2025-10-20 12:26:24 +0700

    feat: snapshot

--- src/snapshot.ts
+++ src/snapshot.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import fs from 'fs';
-import backstop, { Config } from 'backstopjs';
+import { Config } from 'backstopjs';
 import chalk from 'chalk';
 import { BackstopReport, HtmlReportSummary } from './types';
 
